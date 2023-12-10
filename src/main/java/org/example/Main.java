a454740be4990f8404ac31cf2f54ad6dcde0245b
Smertex <kiratsvetkova102@gmail.com>
2023-12-10 21:32:05 +0300

    Редактирую ObjWriter. Добавил возможность записывать в файл вершины

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,29 @@
 package org.example;
 
+import org.example.Math.Vector3f;
+import org.example.ObjWriter.ObjWriter;
 import org.example.ObjWriter.ObjWriterException;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        ObjWriter objWriter = new ObjWriter();
 
+        ArrayList<Vector3f> vertices = new ArrayList<>();
+        vertices.add(new Vector3f(0.2f, 0.3f, 0.5f));
+        vertices.add(new Vector3f(0.3f, 0.4f, 0.6f));
 
+        try {
+            objWriter.recordVertices("C:/Users/kirat/Desktop/Test" + ".obj", vertices);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        //"C:/Users/kirat/Desktop/Test" + ".obj"
 
     }
 }
--- src/main/java/org/example/ObjWriter/ObjWriter.java
+++ src/main/java/org/example/ObjWriter/ObjWriter.java
@@ -1,6 +1,11 @@
 package org.example.ObjWriter;
 
+import org.example.Math.Vector3f;
+
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class ObjWriter {
     private static final String OBJ_VERTEX_TOKEN = "v";
@@ -8,12 +13,43 @@ public class ObjWriter {
     private static final String OBJ_NORMAL_TOKEN = "vn";
     private static final String OBJ_FACE_TOKEN = "f";
 
-    public static File recordFromAFile(){
-        return null;
+    //Записывает информацию из необходимого файла в формат .obj
+    public void recordFromAFile(String fileForRecord, String initialFile){
     }
 
-    public static File recordManual(){
-        return null;
+    //Добавление вершин вручную в необходимый файл. В качестве файла принимает ссылку
+    public void recordVertices(String objFile, ArrayList<Vector3f> vertices) throws IOException {
+        FileWriter fileWriter;
+
+        try {
+            if (objFile.toLowerCase().endsWith(".obj"))
+                fileWriter = new FileWriter(objFile, true);
+            else
+                throw new ObjWriterException("The format of this file is not .obj");
+        }
+        catch (IOException e) {
+            System.err.println("This file not found");
+            throw new RuntimeException(e);
+        }
+        catch (ObjWriterException e) {
+            throw new RuntimeException(e);
+        }
+        for(int i = 0; i < vertices.size(); i++) {
+            if(vertices.get(i) == null) {
+                try {
+                    int errorLine = i + 1;
+                    throw new ObjWriterException("The element is null in the line:" + " " + errorLine);
+                }
+                catch (ObjWriterException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            else {
+                fileWriter.write(OBJ_VERTEX_TOKEN + " " + vertices.get(i).getX() + " " + vertices.get(i).getY() + " " + vertices.get(i).getZ() + "\n");
+            }
+        }
+        fileWriter.flush();
+        fileWriter.close();
     }
 
 
